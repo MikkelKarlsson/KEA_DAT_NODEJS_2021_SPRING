eb7de151bbd9e0d17cae7596406c2fd4db2e349c
MikkelKarlsson <55698695+MikkelKarlsson@users.noreply.github.com>
2021-03-23 15:13:58 +0100

    almost done!

--- 1/4.Express_Wide_Web/app.js
+++ 1/4.Express_Wide_Web/app.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const app = express();
+app.use('/assets/css', express.static(__dirname + '/assets/css'));
 
 console.log(__dirname);
 
 app.get("/", (req, res) => {
     // in node you have __dirname__
     // to serve an html file you have the res.sendFile method
-    res.sendFile(__dirname + "/public/welcome.html");
+    res.sendFile(__dirname + "/public/welcome/welcome.html");
 })
 
 app.get("/dragons", (req, res) => {
-    res.sendFile(__dirname + "/public/dragons.html");
+    res.sendFile(__dirname + "/public/dragons/dragons.html");
 })
+
+app.get("/potato", (req, res) => {
+    if (req.query.value === "spud") {
+        return res.send({ type: "petite potato" });
+    }
+    return res.send({ lifePhilosophy: "My life is potato. I have potato blood in my veins." });
+});
+
 
 const PORT = process.env.PORT || 8080;
 
